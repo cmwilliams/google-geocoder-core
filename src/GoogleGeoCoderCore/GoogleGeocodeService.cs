9e6d91279ba0589758fb60758df767c19e4b0df7
Mike Williams <mike.williams@bluesombrero.com>
2016-11-21 13:57:09 -0500

    Initial Commit

---
+++ src/GoogleGeoCoderCore/GoogleGeocodeService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GoogleGeoCoderCore
+{
+    public class GoogleGeocodeService
+    {
+        public async Task<Location> GeocodeLocation(string address)
+        {
+            //Set default values
+            var location = new Location {Latitude = 0, Longitude = 0};
+
+            try
+            {
+                var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(address));
+
+                using (var client = new HttpClient())
+                {
+                    var request = await client.GetAsync(requestUri);
+                    var content = await request.Content.ReadAsStringAsync();
+                    var xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(content);
+
+                    //We have data!
+                    location.Latitude = double.Parse(xmlDoc.SelectSingleNode("//geometry/location/lat").InnerText, NumberFormatInfo.InvariantInfo);
+                    location.Longitude = double.Parse(xmlDoc.SelectSingleNode("//geometry/location/lng").InnerText, NumberFormatInfo.InvariantInfo);
+                    
+                    //Let's return that data
+                    return location;
+                }
+            }
+            catch (Exception)
+            {
+                //Return default values
+                return location;
+            }
+        }
+    }
+}
---
+++ test/GoogleGeoCoderCore.Tests/GoogleGeocodeTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace GoogleGeoCoderCore.Tests
+{
+    public class GoogleGeocodeTests
+    {
+        [Fact]
+        public async Task Should_Return_Lat_And_Long()
+        {
+            var geocoder = new GoogleGeocodeService();
+            var result = await geocoder.GeocodeLocation("Atlanta, GA");
+            Assert.NotNull(result.Latitude);
+            Assert.NotNull(result.Longitude);
+        }
+
+        [Fact]
+        public async Task Should_Return_Default_Values_For_Lat_And_Long()
+        {
+            var geocoder = new GoogleGeocodeService();
+            var result = await geocoder.GeocodeLocation("dfgfdgfdgfdgdfgdfgdfg");
+            Assert.Equal(0, result.Latitude);
+            Assert.Equal(0,result.Longitude);
+        }
+    }
+}
