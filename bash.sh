5e56555318ee0f66aff8ce688ce49820a654d064
Mike Williams <cmwilliams@gmail.com>
2016-11-21 15:55:08 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,12 @@
+
 #!/usr/bin/env bash
-dotnet restore && dotnet build **/project.json
+
+#exit if any command fails
+set -e
+
+dotnet restore
+
+dotnet build src/GoogleGeoCoderCore
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
